246e8c13aa198f56598793255e2aae3d22cd8be4
Jack Mcleans <jackagwaya@gmail.com>
2019-05-30 20:18:42 +0300

    texture and Surfaces transfererd to texture.cpp

--- graphics.cpp
+++ graphics.cpp
@@ -1,2 +1,45 @@
 #include "graphics.hpp"
+#include <iostream>
 
+bool Graphics::init(){
+    bool successs = true;
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0){
+        std::cout<<"Failed to initialize SDL, ERROR: "<<SDL_GetError()<<std::endl;
+        successs = false;
+    }
+    return successs;
+}
+
+bool Graphics::createWindowAndtexture(){
+    bool success = true;
+
+    window = SDL_CreateWindow("NEED FOR SPEED",
+                                SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED,
+                                WIDTH,
+                                HEIGHT,
+                                SDL_WINDOW_SHOWN
+                                );
+
+    if(window==NULL){
+        std::cout<<"Failed to create window, ERROR: "<<SDL_GetError()<<std::endl;
+        success=false;
+        }
+    
+    renderer = SDL_CreateRenderer(window,
+                                    -1,
+                                    SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+
+    if(renderer==NULL){
+        std::cout<<"Failed to create the renderer, ERROR: "<<SDL_GetError()<<std::endl;
+        success=false;
+        }
+    
+    return success;
+
+}
+
+Graphics::~Graphics(){
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+}
--- graphics.hpp
+++ graphics.hpp
@@ -1,6 +1,29 @@
 #ifndef _GRAPHICS_HPP
 #define _GRAPHICS_HPP
 
+#include <SDL.h>
+
+class Graphics{
+    private:
+    private:
+    public:
+
+    const int WIDTH = 860;
+    const int HEIGHT = 640;
+    SDL_Window * window = NULL;
+    SDL_Renderer* renderer = NULL;
+  
+ 
+
+
+    public:
+    
+    bool init();
+    bool createWindowAndtexture();
+    ~Graphics();
+
+};
+
 
 
 
